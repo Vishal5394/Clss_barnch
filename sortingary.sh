ed2cae41f05350a3768c4619af8bc086653dfaa4
vishal5394 <vishaljadyal@gmail.com>
2022-05-11 23:37:11 +0530

    Take three user input

--- sortingary.sh
+++ sortingary.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Welcome to shell scripting program sorting an arrey"
+
+read -p "enter the value of a:" a;
+read -p "enter the value of b:" b;
+read -p "enter the value of c:" c;
+
