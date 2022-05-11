99f049f643e4077342c6d94a978b6301e57ee8a3
vishal5394 <vishaljadyal@gmail.com>
2022-05-11 23:46:26 +0530

    a+b*c computation

--- sortingary.sh
+++ sortingary.sh
@@ -1,3 +1,11 @@
+
 #!/bin/bash
 
 echo "Welcome to shell scripting program sorting an arrey"
+
+read -p "enter the value of a:" a;
+read -p "enter the value of b:" b;
+read -p "enter the value of c:" c;
+
+exp [0] = $(( $a+$b*$c ));
+echo ${expr[0]};
